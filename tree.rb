0ec1acb07d1990133dc2f3cc267c7c57f8aa34e0
Anton Petrov <antonpetrov@Admins-MacBook-Pro.local>
2013-04-06 17:59:07 +0400

    Initial

--- tree.rb
+++ tree.rb
@@ -1,52 +1,60 @@
-class Print_tree
+class Tree
+  def print(center, height)
+    @center = center + height * 3
+    @height = height
+    print_top_section
+    print_middle_section
+    print_lower_section
+    print_stem
+  end
 
- def initialize
-  clear_screen
+  def print_top_section
+    print_triangle(0, @height - 1)
+  end
 
-  center, height = ask_user_center_and_height_of_tree
+  def print_middle_section
+    print_triangle(1, @height)
+  end
 
-  print_tree_with center, height
- end
+  def print_lower_section
+    print_triangle(2, @height + 1)
+  end
 
- def clear_screen
-  print "\e[2J\e[f"
- end
+  def print_stem
+    1.upto(@height) { |i| puts spaces(@center - (@height > 2 ? 1 : 0)) + stars(@height > 2 ? 3 : 1) }
+  end
 
- def one_or_more(number)
-  return number < 1 ? 1 : number
- end
+  def print_triangle(up_size, down_size)
+    up_size.upto(down_size) { |i| puts spaces(@center - i) + stars(1 + (i * 2)) }
+  end
 
- def ask_user_center_and_height_of_tree
-  print "Tree height(always will be multiplied by three): "
+  def spaces(amount)
+    " " * amount
+  end
+
+  def stars(amount)
+    "*" * amount
+  end
+end
+
+def one_or_more(number)
+  number < 1 ? 1 : number
+end
+
+def ask_user_center_and_height_of_tree
+  print "Tree height (always will be multiplied by three): "
   height = one_or_more(gets.to_i)
-  print "Center of the tree(the distance from the left side of the screen): "
+  print "Center of the tree (the distance from the left side of the screen): "
   center = one_or_more(gets.to_i)
   return center, height
- end
+end
 
- def print_tree_with(center, height)
-  center += height * 3
-  print_treeangle( up_size: 0, down_size: height - 1, center: center )
-  0.upto(height - 1) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
-  1.upto(height    ) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
-  2.upto(height + 1) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
-  1.upto(height) { |i| puts ( " " * (center - ( height > 2 ? 1 : 0)) + ("*" * (height > 2 ? 3 : 1)) ) }
- end
-
- def print_treeangle(arg = {})
-  up_size, down_size, center = arg[:up_size].to_i, arg[:down_size].to_i, arg[:center].to_i
-  up_size.upto(down_size) { |i| puts spaces(center - i) + stars(1+(i * 2)) }
- end
-
- def spaces(amount)
-  return " " * amount
- end
-
- def stars(amount)
-  return "*" * amount
- end
-
+def clear_screen
+  print "\e[2J\e[f"
 end
 
 
-Print_tree.new
+center, height = ask_user_center_and_height_of_tree
+clear_screen
+
+Tree.new.print(center, height)
